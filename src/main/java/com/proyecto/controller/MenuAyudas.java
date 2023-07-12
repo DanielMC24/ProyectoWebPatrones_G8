43b493f0e7ceb42326a2d4d638192ee957ef3e0d
Usuario <Usuario@Daniel-Escritorio>
2023-07-11 20:46:27 -0600

    Prueba

---
+++ src/main/java/com/proyecto/controller/MenuAyudas.java
@@ -0,0 +1,18 @@
+
+package com.proyecto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MenuAyudas {
+    
+    @RequestMapping("/menuAyudas")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "MenuAyudas";
+    }
+    
+}
